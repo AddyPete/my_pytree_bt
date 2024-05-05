f376cd69677d4068205b577fcee0a330818b8209
Adrian Pete Lagare <aprlagare1999@gmail.com>
2024-05-06 02:07:57 +0800

    enchanced stuffs

--- my_pytree_bt/pytree_real_gears_node_no_bb.py
+++ my_pytree_bt/pytree_real_gears_node_no_bb.py
@@ -3,49 +3,59 @@ import py_trees_ros.trees
 import py_trees.console as console
 import rclpy
 import sys
-import operator
 import numpy as np
 import time
-from py_trees_ros.subscribers import ToBlackboard
 from std_msgs.msg import Bool, String, Float32, Int32
 from py_trees.common import Status
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy
 
+
+# Calculate the other three points
+# BASE_WAYPOINT_OBJECTS = [
+#     [5, 0],
+#     [5, 5],
+#     [0, 5],
+#     [0, 0],
+# ]
 # BASE_WAYPOINT_OBJECTS = [[2.158, 0.141], [2.330, 1.898]]  # OFFICE TEST 2 PTS
 BASE_WAYPOINT_OBJECTS = [
-    [3.33, 0.20],
-    [2.330, 1.898],
-    [1.36, 2.0],
-    [1.84, -0.34],
+    [3.67, 0],
+    [3.69, 1.99],
+    [0.7, 2.31],
 ]  # OFFICE COMPLETE
-#  SQUARE
+
+
 # BASE_WAYPOINT_OBJECTS = [
 #     [10.07, 0],
 #     [10.0, 2.1],
 #     [-0.5, 2.2],
 #     [-0.5, 44],
 # ]  # RAMP PHYSICS
-DIFF_ANGLE_THRESHOLD = 10  # DEGREE
+DIFF_ANGLE_THRESHOLD = 8  # DEGREE
 DISTANCE_TO_TARGET_THRESHOLD = 0.6  # meters
 ARUCO_FINISHED_MISISON_THRESH = 0.6  # meters
 
-LINEAR_VEL = 0.20
-LINEAR_VEL_ON_FLAT = 0.20
+LINEAR_VEL = 0.60
+LINEAR_VEL_ON_FLAT = 0.23
 ANGULAR_VEL = 0.05
-ROTATION_VEL = 0.2
-FLICK_ANGLE = 0.075
+ROTATION_VEL = 0.18
+FLICK_ANGLE = 0.10
 
+# ZED_ODOM_TOPIC = "/zed/zed_node/odom"
 ZED_ODOM_TOPIC = "/zed/zed_node/odom"
 
-PERIOD_MS = 125  # MS
-TREE_DEBUG = True
+
+PERIOD_MS = 50  # MS
+TREE_DEBUG = False
 
 LEFT_OBSTACLE_TOPIC = "/zed/is_obs_near_left"
 RIGHT_OBSTACLE_TOPIC = "/zed/is_obs_near_right"
 # LINEAR_VEL = 0.0
 # ANGULAR_VEL = 0.0
+
+BASE_WAYPOINT_OBJECTS_STATUS = [False] * len(BASE_WAYPOINT_OBJECTS)
 
 
 class ModePublisher(py_trees.behaviour.Behaviour):
@@ -88,10 +98,11 @@ class ModePublisher(py_trees.behaviour.Behaviour):
 
 
 class CheckVisitedWaypoint(py_trees.behaviour.Behaviour):
-    def __init__(self, name, waypoint_ind):
+    def __init__(self, name, waypoint, waypoint_ind):
         super(CheckVisitedWaypoint, self).__init__(name)
-        self.blackboard = py_trees.blackboard.Blackboard()
+        # self.blackboard = py_trees.blackboard.Blackboard()
         self.wp_ind = waypoint_ind - 1
+        self.action_policy = ActionPolicy(waypoint)
 
     def setup(self, **kwargs):
 
@@ -106,20 +117,90 @@ class CheckVisitedWaypoint(py_trees.behaviour.Behaviour):
         self.node.get_logger().info(f"Setup {self.__class__.__name__}")
 
         self.cmd_vel_publisher_ = self.node.create_publisher(Twist, "cmd_vel", 10)
+        self.wp_pstat_ublisher = self.node.create_publisher(
+            String, "waypoint_status", 10
+        )
         self.cmd_vel_msg = Twist()
+        self.aruco_dist_subscriber = self.node.create_subscription(
+            Float32, "/aruco_dist_x", self.aruco_dist_sub_callback, 10
+        )
+
+        self.aruco_id_subscriber = self.node.create_subscription(
+            Int32, "/aruco_id", self.aruco_id_sub_callback, 10
+        )
+
+        # self.dist_to_target_subscriber = self.node.create_subscription(
+        #     Float32, "/dist_to_target", self.dist_sub_callback, 10
+        # )
+        self.zed_odom_subscriber = self.node.create_subscription(
+            Odometry,
+            ZED_ODOM_TOPIC,  # Change 'odometry_topic' to the actual topic name
+            self.odometry_callback,
+            10,  # Adjust queue size as per your requirement
+        )
+        (
+            self.x_pos,
+            self.y_pos,
+            self.z_pos,
+            self.x_ori,
+            self.y_ori,
+            self.z_ori,
+            self.w_ori,
+        ) = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+        self.det_aruco_dis = -1
+        # self.dist_to_target = -1
+        self.det_aruco_id = 0
+
+    def odometry_callback(self, msg):
+        # Extracting position
+        self.x_pos = msg.pose.pose.position.x
+        self.y_pos = msg.pose.pose.position.y
+        self.z_pos = msg.pose.pose.position.z
+
+        # Extracting orientation (rotation)
+        self.x_ori = msg.pose.pose.orientation.x
+        self.y_ori = msg.pose.pose.orientation.y
+        self.z_ori = msg.pose.pose.orientation.z
+        self.w_ori = msg.pose.pose.orientation.w
+
+    def aruco_id_sub_callback(self, msg):
+        self.det_aruco_id = msg.data
+
+    def aruco_dist_sub_callback(self, msg):
+        # pass
+        self.det_aruco_dis = msg.data
 
     def initialise(self):
         self.node.get_logger().info(f"Init {self.__class__.__name__}")
 
     def update(self):
-        self.visited_waypoints = self.blackboard.get("visited_waypoints")
+        self.wp_pstat_ublisher.publish(String(data=str(BASE_WAYPOINT_OBJECTS_STATUS)))
+
+        rover_action, finished_mission, distance_to_target = (
+            self.action_policy.get_action_policy(
+                self.x_pos,
+                self.y_pos,
+                self.z_pos,
+                self.x_ori,
+                self.y_ori,
+                self.z_ori,
+                self.w_ori,
+            )
+        )
+        if finished_mission or self.is_aruco_finish_mission():
+            self.node.get_logger().info(f"WP {self.wp_ind+1}: >>> SUCCESS!!!")
+            BASE_WAYPOINT_OBJECTS_STATUS[self.wp_ind] = True
+
+        # self.visited_waypoints = self.blackboard.get("visited_waypoints")
 
         self.node.get_logger().info(f"Update {self.__class__.__name__}")
 
-        is_visited = self.visited_waypoints["wp_" + str(self.wp_ind + 1)]
+        # is_visited = self.visited_waypoints["wp_" + str(self.wp_ind + 1)]
+        is_visited = BASE_WAYPOINT_OBJECTS_STATUS[self.wp_ind]
 
         self.node.get_logger().info(
-            f"Update, Is WP {self.wp_ind} Visited? {is_visited}"
+            f"Update, Is WP {self.wp_ind +1} Visited? {is_visited}"
         )
 
         # self.visited_waypoints["wp_" + str(self.wp_inf + 1)]
@@ -130,6 +211,14 @@ class CheckVisitedWaypoint(py_trees.behaviour.Behaviour):
             return py_trees.common.Status.SUCCESS
         return py_trees.common.Status.FAILURE
 
+    def is_aruco_finish_mission(self):
+        if (
+            self.det_aruco_dis > 0.0
+            and self.det_aruco_dis <= ARUCO_FINISHED_MISISON_THRESH
+        ) and (self.wp_ind + 1 == self.det_aruco_id):
+            return True
+        return False
+
     def terminate(self, new_status):
         self.node.get_logger().info(
             f"Terminate {self.__class__.__name__} - {new_status}"
@@ -137,9 +226,10 @@ class CheckVisitedWaypoint(py_trees.behaviour.Behaviour):
 
 
 class Stop(py_trees.behaviour.Behaviour):
-    def __init__(self, name):
+    def __init__(self, name, flick=False):
         super(Stop, self).__init__(name)
         # self.blackboard = py_trees.blackboard.Blackboard()
+        self.flick = flick
 
     def setup(self, **kwargs):
 
@@ -160,6 +250,12 @@ class Stop(py_trees.behaviour.Behaviour):
     def update(self):
         self.node.get_logger().info(f"Update {self.__class__.__name__}")
 
+        if self.flick:
+            self.cmd_vel_msg.linear.x = -0.2
+            self.cmd_vel_msg.angular.z = 0.0
+            self.cmd_vel_publisher_.publish(self.cmd_vel_msg)
+
+        time.sleep(0.5)
         self.cmd_vel_msg.linear.x = 0.0
         self.cmd_vel_msg.angular.z = 0.0
         self.cmd_vel_publisher_.publish(self.cmd_vel_msg)
@@ -227,7 +323,7 @@ class CheckWall(py_trees.behaviour.Behaviour):
 class CheckSideWall(py_trees.behaviour.Behaviour):
     def __init__(self, name):
         super(CheckSideWall, self).__init__(name)
-        self.blackboard = py_trees.blackboard.Blackboard()
+        # self.blackboard = py_trees.blackboard.Blackboard()
 
     def setup(self, **kwargs):
 
@@ -268,6 +364,18 @@ class CheckSideWall(py_trees.behaviour.Behaviour):
         self.angle_requested_go_left_publisher_ = self.node.create_publisher(
             Bool, "angle_requested_go_left", 10
         )
+
+        self.wall_collision_subscriber = self.node.create_subscription(
+            Bool, "/is_near_wall", self.laser_wall_collision_callback, 10
+        )
+        self.zed_wall_collision_subscriber = self.node.create_subscription(
+            Bool, "/zed/is_near_wall", self.zed_wall_collision_callback, 10
+        )
+
+        self.obj_detection_subscriber = self.node.create_subscription(
+            Bool, "/is_object_detected", self.obj_detected_callback, 10
+        )
+
         self.cmd_vel_publisher_ = self.node.create_publisher(Twist, "cmd_vel", 10)
 
         self.is_near_left_wall = False
@@ -279,8 +387,23 @@ class CheckSideWall(py_trees.behaviour.Behaviour):
         self.is_flat = True
         self.curr_angle = 0.0
 
+        self.is_obj_detected, self.is_near_wall, self.is_near_wall_zed = (
+            False,
+            False,
+            False,
+        )
+
         self.cmd_vel_publisher_ = self.node.create_publisher(Twist, "cmd_vel", 10)
         self.cmd_vel_msg = Twist()
+
+    def laser_wall_collision_callback(self, msg):
+        self.is_near_wall = msg.data
+
+    def zed_wall_collision_callback(self, msg):
+        self.is_near_wall_zed = msg.data
+
+    def obj_detected_callback(self, msg):
+        self.is_obj_detected = msg.data
 
     def lidar_wall_left_collision_callback(self, msg):
         self.is_lidar_near_left_wall = msg.data
@@ -302,8 +425,19 @@ class CheckSideWall(py_trees.behaviour.Behaviour):
 
     def update(self):
 
-        self.node.get_logger().info(f"Near Left Wall: {self.is_near_left_wall}")
-        self.node.get_logger().info(f"Near Right Wall: {self.is_near_right_wall}")
+        self.node.get_logger().info(f"Near Left Wall Zed: {self.is_near_left_wall}")
+        self.node.get_logger().info(f"Near Right Wall Zed: {self.is_near_right_wall}")
+
+        self.node.get_logger().info(
+            f"Near Left Wall Lidar: {self.is_lidar_near_left_wall}"
+        )
+        self.node.get_logger().info(
+            f"Near Right Wall Lidar: {self.is_lidar_near_right_wall}"
+        )
+
+        if self.is_near_wall or self.is_near_wall_zed:
+            # self.cmd_vel_publisher_.publish(self.cmd_vel_msg)
+            return Status.FAILURE
 
         if self.is_flat:
             self.cmd_vel_msg.linear.x = LINEAR_VEL_ON_FLAT
@@ -312,12 +446,14 @@ class CheckSideWall(py_trees.behaviour.Behaviour):
         # self.cmd_vel_msg.angular.z = 0.0
 
         if self.is_near_left_wall or self.is_lidar_near_left_wall:
+            self.node.get_logger().info("NEAR LEFT!!!!!!!!")
             self.angle_requested_go_right_publisher_.publish(Bool(data=True))
             self.cmd_vel_msg.angular.z = -FLICK_ANGLE
             self.cmd_vel_publisher_.publish(self.cmd_vel_msg)
             return Status.RUNNING
 
         if self.is_near_right_wall or self.is_lidar_near_right_wall:
+            self.node.get_logger().info("NEAR RIGHT!!!!!!!!")
             self.angle_requested_go_left_publisher_.publish(Bool(data=True))
             self.cmd_vel_msg.angular.z = FLICK_ANGLE
             self.cmd_vel_publisher_.publish(self.cmd_vel_msg)
@@ -437,6 +573,8 @@ class ActionPolicy:
 
         if distance_to_target < DISTANCE_TO_TARGET_THRESHOLD:
             self.finished_mission = True
+        else:
+            self.finished_mission = False
 
         print(f"{distance_to_target} - {rover_action}")
 
@@ -446,11 +584,14 @@ class ActionPolicy:
 class GoToWayPoint(py_trees.behaviour.Behaviour):
     def __init__(self, name, waypoint, wp_ind):
         super(GoToWayPoint, self).__init__(name)
-        self.blackboard = py_trees.blackboard.Blackboard()
+        # self.blackboard = py_trees.blackboard.Blackboard()
 
         # self.waypoint = waypoint
         self.name = name
         self.wp_ind = wp_ind - 1
+
+        self.finished_mission = False
+
         self.action_policy = ActionPolicy(waypoint)
 
     def setup(self, **kwargs):
@@ -546,6 +687,10 @@ class GoToWayPoint(py_trees.behaviour.Behaviour):
         self.angle_requested_go_left_publisher_ = self.node.create_publisher(
             Bool, "angle_requested_go_left", 10
         )
+
+        self.wp_pstat_ublisher = self.node.create_publisher(
+            String, "waypoint_status", 10
+        )
         self.is_near_left_wall = False
         self.is_near_right_wall = False
 
@@ -556,8 +701,6 @@ class GoToWayPoint(py_trees.behaviour.Behaviour):
 
         self.det_aruco_id = 0
         self.det_aruco_dis = -1
-
-        self.finished_mission = False
 
     def aruco_id_sub_callback(self, msg):
         self.det_aruco_id = msg.data
@@ -606,23 +749,25 @@ class GoToWayPoint(py_trees.behaviour.Behaviour):
     def initialise(self):
 
         self.node.get_logger().info(f"Init {self.__class__.__name__}")
-        self.visited_waypoints = self.blackboard.get("visited_waypoints")
+        # self.visited_waypoints = self.blackboard.get("visited_waypoints")
 
     def update(self):
 
-        self.node.get_logger().info(f"Near Wall Zed {self.is_near_wall_zed}")
+        # self.node.get_logger().info(f"Near Wall Zed {self.is_near_wall_zed}")
+        self.wp_pstat_ublisher.publish(String(data=str(BASE_WAYPOINT_OBJECTS_STATUS)))
 
         self.node.get_logger().info(
             f"Aruco: {self.is_aruco_finish_mission()} | Finished_mission {self.finished_mission}"
         )
         if self.finished_mission or self.is_aruco_finish_mission():
-            self.node.get_logger().info(f"SUCCESS!!!")
-            self.visited_waypoints["wp_" + str(self.wp_ind + 1)] = True
-            self.blackboard.set("visited_waypoints", self.visited_waypoints)
-            # linear_vel, angular_vel = 0.0, 0.0
-            # self.cmd_vel_msg.linear.x = 0.0
-            # self.cmd_vel_msg.angular.z = 0.0
-            # self.cmd_vel_publisher_.publish(self.cmd_vel_msg)
+            self.node.get_logger().info(f"WP {self.wp_ind+1}: >>> SUCCESS!!!")
+            BASE_WAYPOINT_OBJECTS_STATUS[self.wp_ind] = True
+            # self.visited_waypoints["wp_" + str(self.wp_ind + 1)] = True
+            # self.blackboard.set("visited_waypoints", self.visited_waypoints)
+            linear_vel, angular_vel = 0.0, 0.0
+            self.cmd_vel_msg.linear.x = 0.0
+            self.cmd_vel_msg.angular.z = 0.0
+            self.cmd_vel_publisher_.publish(self.cmd_vel_msg)
             return py_trees.common.Status.SUCCESS
 
         if self.is_obj_detected or self.is_near_wall or self.is_near_wall_zed:
@@ -645,7 +790,7 @@ class GoToWayPoint(py_trees.behaviour.Behaviour):
         self.node.get_logger().info(f"Update  {self.__class__.__name__} - {self.name}")
         self.angle_requested_go_right_publisher_.publish(Bool(data=False))
         self.angle_requested_go_left_publisher_.publish(Bool(data=False))
-        rover_action, self.finished_mission, distance_to_target = (
+        rover_action, finished_mission, distance_to_target = (
             self.action_policy.get_action_policy(
                 self.x_pos,
                 self.y_pos,
@@ -656,9 +801,10 @@ class GoToWayPoint(py_trees.behaviour.Behaviour):
                 self.w_ori,
             )
         )
+        self.finished_mission = finished_mission
 
         self.node.get_logger().info(
-            f"Go To WP Act {self.wp_ind + 1}: {rover_action} Finished Mission?: {self.finished_mission}"
+            f"Go To WP Act {self.wp_ind + 1}: {rover_action} Finished Mission?: {finished_mission}"
         )
 
         if rover_action == "Left Steer":
@@ -682,7 +828,7 @@ class GoToWayPoint(py_trees.behaviour.Behaviour):
         self.cmd_vel_publisher_.publish(self.cmd_vel_msg)
         self.dist_to_target_pub.publish(Float32(data=distance_to_target))
         self.curr_waypoint_pub.publish(Int32(data=self.wp_ind + 1))
-        self.curr_waypoint_status_pub.publish(Bool(data=self.finished_mission))
+        self.curr_waypoint_status_pub.publish(Bool(data=finished_mission))
 
         return py_trees.common.Status.RUNNING
         # except Exception as e:
@@ -709,7 +855,7 @@ class GoToWayPoint(py_trees.behaviour.Behaviour):
 class RotateToWaypoint(py_trees.behaviour.Behaviour):
     def __init__(self, name, waypoint):
         super(RotateToWaypoint, self).__init__(name)
-        self.blackboard = py_trees.blackboard.Blackboard()
+        # self.blackboard = py_trees.blackboard.Blackboard()
 
         # self.waypoint = waypoint
         self.name = name
@@ -836,7 +982,9 @@ class RotateToWaypoint(py_trees.behaviour.Behaviour):
 def create_core_wp_subtree(waypoint_index, waypoint):
 
     guard_wp_checker = CheckVisitedWaypoint(
-        name=f"Guard WP Check {waypoint_index}", waypoint_ind=waypoint_index
+        name=f"Guard WP Check {waypoint_index}",
+        waypoint=waypoint[waypoint_index - 1],
+        waypoint_ind=waypoint_index,
     )
 
     check_wp_before_moving = py_trees.composites.Selector(
@@ -852,7 +1000,7 @@ def create_core_wp_subtree(waypoint_index, waypoint):
     is_near_wall = CheckWall(name=f"Check Wall {waypoint_index}")
 
     stop_action_for_obj_det = Stop(name=f"Stop Obj{waypoint_index}")
-    stop_action_for_rotate = Stop(name=f"Stop Spin{waypoint_index}")
+    stop_action_for_rotate = Stop(name=f"Stop Spin{waypoint_index}", flick=True)
     stop_action_for_mirrored = Stop(name=f"Stop mirrored {waypoint_index}")
 
     mov_to_wp = GoToWayPoint(
@@ -887,7 +1035,7 @@ def create_core_wp_subtree(waypoint_index, waypoint):
         name=f"Stop and Spin {waypoint_index}", memory=True
     )
 
-    stop_final = Stop(name=f"Final Stop")
+    # stop_final = Stop(name=f"Final Stop")
 
     wall_detection.add_children([is_near_wall, stop_and_spin])
     stop_and_spin.add_children(
@@ -912,13 +1060,13 @@ def create_core_wp_subtree(waypoint_index, waypoint):
         [obj_detection, wall_detection, check_side_wall, mov_to_wp]
     )
 
-    check_wp_before_moving.add_children([guard_wp_checker, wp_sequence, stop_final])
+    check_wp_before_moving.add_children([guard_wp_checker, wp_sequence])
 
     return check_wp_before_moving
 
 
 def create_root() -> py_trees.behaviour.Behaviour:
-    blackboard = py_trees.blackboard.Blackboard()
+    # blackboard = py_trees.blackboard.Blackboard()
 
     root = py_trees.composites.Parallel(
         name="Root",
@@ -929,7 +1077,7 @@ def create_root() -> py_trees.behaviour.Behaviour:
         "wp_" + str(i + 1): False for i in range(len(BASE_WAYPOINT_OBJECTS))
     }
 
-    blackboard.set("visited_waypoints", visited_waypoints)
+    # blackboard.set("visited_waypoints", visited_waypoints)
 
     go_to_waypoints = py_trees.composites.Sequence(name="Go to Waypoints", memory=True)
 
@@ -938,8 +1086,6 @@ def create_root() -> py_trees.behaviour.Behaviour:
     for index, _ in enumerate(BASE_WAYPOINT_OBJECTS, start=1):
         go_to_waypoints.add_child(create_core_wp_subtree(index, BASE_WAYPOINT_OBJECTS))
 
-    # go_to_waypoints.add_child(stop_final)
-    # root.add_child(stop_final)
     return root
 
 
